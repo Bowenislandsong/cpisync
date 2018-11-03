a082207e1880baf73a91d6ea74e9c918751a8207
Bowen Song <bowenislandsong@gmail.com>
2018-11-03 17:10:02 -0400

    OMP working

--- tests/unit/kshinglingTest.cpp
+++ tests/unit/kshinglingTest.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Bowen Song on 9/16/18.
 //
-
+#include <omp.h>
 #include <climits>
 #include "kshinglingTest.h"
 #include "time.h"
@@ -24,14 +24,16 @@ void kshinglingTest::testAll() {
     // init a string of random byte (shortest,longest) possible string len
     string Alicetxt = randAsciiStr(1000000);  // generate a string
     //string Bobtxt = randStringEdit(Alicetxt,10);  // Generate a edited string
-
+K_Shingle Alice = K_Shingle(log2(Alicetxt.size()),Alicetxt);
+omp_set_num_threads(2);
+#pragma omp parallel for 
+ for (int i = 0; i<10;++i){
     clock_t t1 = clock();
-    K_Shingle Alice = K_Shingle(log2(Alicetxt.size()),Alicetxt);
-
     auto AliceStringNOrder = Alice.reconstructStringBacktracking();  // Get order of the cycle
     clock_t t2 = clock();
     cout<<to_string(double(t2-t1)/CLOCKS_PER_SEC)<<endl;
     //test functions
+
     CPPUNIT_ASSERT(AliceStringNOrder.first==Alicetxt);  // Make sure Alice can recover her own string from shingle set
     CPPUNIT_ASSERT(AliceStringNOrder.second > 0);  //Make sure string order is not 0 (default)
     //CPPUNIT_ASSERT(Alicetxt!=Bobtxt);
@@ -43,7 +45,7 @@ void kshinglingTest::testAll() {
     CPPUNIT_ASSERT(Alice.getOriginString()==AliceStringNOrder.first);
     CPPUNIT_ASSERT(Alice.getShingleSet().size()>0);
 
-
+}
     //test with Half round sync
 
 }
